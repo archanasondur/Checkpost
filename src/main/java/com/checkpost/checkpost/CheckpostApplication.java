2ff4070c51eccbbb480bd9e42439a414e4643dc3
archanasondur <aksondur@gmail.com>
2026-07-14 18:46:55 -0700

    Part 0: Spring Boot skeleton, Postgres + Redis via Docker Compose

---
+++ src/main/java/com/checkpost/checkpost/CheckpostApplication.java
@@ -0,0 +1,13 @@
+package com.checkpost.checkpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkpost/checkpost/CheckpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkpost.checkpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
