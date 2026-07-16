457e873b3886d7cb77124fc5d4b6b756168fcf06
archanasondur <aksondur@gmail.com>
2026-07-15 23:47:27 -0700

    small import fix in the CheckpostIntegrationTest.java file

--- src/test/java/com/checkpost/checkpost/CheckpostIntegrationTest.java
+++ src/test/java/com/checkpost/checkpost/CheckpostIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.checkpost.checkpost;
 
 import tools.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
